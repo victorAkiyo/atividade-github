c909e586924dfe6b0b4c295ed7ec2a75217c9a3c
victorAkiyo <victoryamashita09@gmail.com>
2026-05-18 20:24:45 -0300

    issue#4resolvida

--- Vanilla-Js-ToDoList-main/app.js
+++ Vanilla-Js-ToDoList-main/app.js
@@ -21,7 +21,7 @@ function addTodo(e) {
   saveLocalTodos(todoInput.value);
   newTodo.classList.add("todo-item");
   todoDiv.appendChild(newTodo);
-  todoInput.value = "";
+  todoInput.value = ""
   
   // Create Completed Button
   const completedButton = document.createElement("button");
